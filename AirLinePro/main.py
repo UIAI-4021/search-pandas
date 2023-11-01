35db53e4b00e0d95a57d5b2a12cb36106e93c434
Kianoosh <kia.comp.eng_outlook.com>
2023-11-01 21:04:12 +0330

    graph

--- AirLinePro/main.py
+++ AirLinePro/main.py
@@ -1,9 +1,42 @@
-import pandas
+import pandas as pd
+from Flight import Flight
+from Vertex import VertexClass
+from Graph import Graph
+from Edge import EdgeClass
+df = pd.read_csv('../assets/files/Flight_Data_Reduced.csv')
 
-import MySetting as ms
-ms.MySetting.print_animated('Welcome to our app')
+def set_data(records):
+    flights = list()
+    for index, record in df.iterrows():
+        f = Flight(record['Airline'] , record['SourceAirport'] ,record['DestinationAirport']
+               ,record['SourceAirport_City'] ,record['SourceAirport_Country']
+               ,record['SourceAirport_Latitude'] ,record['SourceAirport_Longitude']
+               , record['SourceAirport_Altitude'] ,record['DestinationAirport_City']
+               ,record['DestinationAirport_Country'] ,record['DestinationAirport_Latitude']
+               ,record['DestinationAirport_Longitude'] ,record['DestinationAirport_Altitude']
+               ,record['Distance'] ,record['FlyTime'] ,record['Price'])
+        flights.append(f)
+    return flights
 
-# inp = input()
-# inps = inp.split(' - ')
-# print(inps)
-#
+
+
+flights = set_data(df)
+
+
+graph = Graph.getInstance()
+for flight in flights:
+    if graph.get_vertex(flight.SourceAirport) is None:
+        graph.add_vertex(VertexClass(flight.SourceAirport))
+    if graph.get_vertex(flight.DestinationAirport) is None:
+        graph.add_vertex(VertexClass(flight.DestinationAirport))
+
+for i , record in df.iterrows():
+    starting = graph.get_vertex(str(record['SourceAirport']))
+    ending = graph.get_vertex(str(record['DestinationAirport']))
+    last_edge = graph.get_edge(starting , ending)
+    if last_edge is None or last_edge.cost > record['Price']:
+        graph.add_edge(starting , ending , cost=record['Price'])
+print()
+
+
+
