9142867c0c22f3a860a5694b03aaf3c4d2062bcb
Kianoosh <kia.comp.eng_outlook.com>
2023-11-03 23:37:31 +0330

    Changing graph to directed graph

--- AirLinePro/Vertex.py
+++ AirLinePro/Vertex.py
@@ -2,9 +2,11 @@ class VertexClass:
 
     def __init__(self,name):
         self.name = name
-        self.connected_vertices = dict()
+        self.startings = dict()
+        self.endings = dict()
 
-    def add_link(self, vertex_to_be_connected , edge):
-        self.connected_vertices[vertex_to_be_connected] = edge
+    def add_ending(self, vertex_to_be_connected , edge):
+        self.endings[vertex_to_be_connected] = edge
 
-
+    def add_starting(self, vertex_to_be_connected, edge):
+        self.startings[vertex_to_be_connected] = edge
