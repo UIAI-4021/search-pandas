d19dab067750dc85cbf71e01d82495ea77ef21de
emika s <mel.shirian@gmail.com>
2023-11-10 12:29:01 +0330

    gradient functions (convergence)

--- AirLinePro/MachineLearning.py
+++ AirLinePro/MachineLearning.py
@@ -100,7 +100,7 @@ class MyML:
 
         return dj_db, dj_dw
     @staticmethod
-    def gradient_descent(X, y, w_in, b_in, cost_function, gradient_function, alpha, num_iters):
+    def gradient_descent(X, y, w_in, b_in, alpha, num_iters):
         """
         Performs batch gradient descent to learn w and b. Updates w and b by taking
         num_iters gradient steps with learning rate alpha
@@ -110,8 +110,6 @@ class MyML:
           y (ndarray (m,))    : target values
           w_in (ndarray (n,)) : initial model parameters
           b_in (scalar)       : initial model parameter
-          cost_function       : function to compute cost
-          gradient_function   : function to compute the gradient
           alpha (float)       : Learning rate
           num_iters (int)     : number of iterations to run gradient descent
 
@@ -121,11 +119,12 @@ class MyML:
           """
 
         # An array to store cost J and w's at each iteration primarily for graphing later
-        J_history = []
+        w_history = []
         w = copy.deepcopy(w_in)  # avoid modifying global w within function
         b = b_in
 
-        for i in range(num_iters):
+        converged = False
+        for i in range(num_iters) and (not converged):
 
             # Calculate the gradient and update the parameters
             dj_db, dj_dw = MyML.compute_gradient(X, y, w, b)
@@ -134,12 +133,17 @@ class MyML:
             w = w - alpha * dj_dw
             b = b - alpha * dj_db
 
-            # Save cost J at each iteration
-            if i < 100000:  # prevent resource exhaustion
-                J_history.append(cost_function(X, y, w, b))
+            w_history.append(w)
 
-            # Print cost every at intervals 10 times or as many iterations if < 10
-            if i % math.ceil(num_iters / 10) == 0:
-                print(f"Iteration {i:4d}: Cost {J_history[-1]:8.2f}   ")
+            converged_tmp = w
+            a = 0
+            for j in range(len(w_history), len(w_history)-100, -1):
+                if abs(converged_tmp - w_history[j]) < 0.00001:
+                    continue
+                else:
+                    a = 1
+            if a == 0:
+                converged = True
 
-        return w, b, J_history  # return final w,b and J history for graphing
+
+        return w, b
